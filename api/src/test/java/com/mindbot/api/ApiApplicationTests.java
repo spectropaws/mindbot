526e01b798f1a68f5b220b36582b115db26234c0
spectropaws <spectropaws777@gmail.com>
2026-03-25 22:37:36 +0530

    init

---
+++ api/src/test/java/com/mindbot/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindbot.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
